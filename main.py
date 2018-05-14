f9ad19d84591ab7330fe76172d9e569db480d28b
Cyan Tarek <cyantarek.cg@gmail.com>
2018-05-15 00:46:39 +0600

    Initial

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder="static")
+
+
+@app.route("/membership/")
+def membership():
+	return render_template("index.html")
+
+
+app.run(debug=True)
