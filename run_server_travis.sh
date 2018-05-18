298eccd3f251d0ffd017465ec2777132755151f5
Cyan Tarek <cyantarek.cg@gmail.com>
2018-05-19 01:25:40 +0600

    membership list added?

---
+++ run_server_travis.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "=============CD INTO==================="
+cd /projects/travis_test
+echo "=============Delete DIR==================="
+rm -rf membership-site-flask
+echo "=============CLONING==================="
+git clone https://github.com/cyantarek/membership-site-flask
+echo "=============CD INTO==================="
+cd membership-site-flask
+echo "=============Kill Port==================="
+fuser -n tcp -k 5001 || true
+echo "=============Run Server==================="
+python3 main.py >/dev/null 2>&1 || true
